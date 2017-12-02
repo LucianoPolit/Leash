30e622e9c212327522a53c0586d94af1c694234c
Luciano Polit <lucianopolit@gmail.com>
2017-12-02 09:27:45 +0000

    Clean some code

--- Source/RxSwift/Rx+Client.swift
+++ Source/RxSwift/Rx+Client.swift
@@ -1,8 +1,25 @@
 //
 //  Rx+Client.swift
-//  Alamofire
 //
-//  Created by Luciano Polit on 12/1/17.
+//  Copyright (c) 2017 Luciano Polit <lucianopolit@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
@@ -31,22 +48,11 @@ extension Reactive where Base: Client {
 private extension Client {
     
     func execute<T: Decodable>(_ endpoint: Endpoint, queue: DispatchQueue? = nil) -> Single<ReactiveResponse<T>> {
-        return Single.create { single in
-            let disposable: Disposable?
-            
-            do {
-                let request = try self.request(for: endpoint)
-                disposable = request.rx
-                    .responseDecodable(queue: queue, client: self, endpoint: endpoint)
-                    .subscribe { event in
-                        single(event)
-                    }
-            } catch {
-                disposable = nil
-                single(.error(error))
-            }
-            
-            return Disposables.create { disposable?.dispose() }
+        do {
+            let request = try self.request(for: endpoint)
+            return request.rx.responseDecodable(queue: queue, client: self, endpoint: endpoint)
+        } catch {
+            return .error(Error.encoding(error))
         }
     }
     
