8d5b8b930f5fb49e6f60837074c82abb041744b9
Luciano Polit <lucianopolit@gmail.com>
2018-07-20 14:20:57 -0300

    Update tests

--- Tests/InterceptorChainTests.swift
+++ Tests/InterceptorChainTests.swift
@@ -67,6 +67,22 @@ extension InterceptorChainTests {
         XCTAssertEqual(count, 1)
     }
     
+    func testFinishFalse() {
+        XCTAssertEqual(count, 0)
+        chain.complete(with: Data(), finish: false)
+        chain.complete(with: Data(), finish: false)
+        chain.complete(with: Data(), finish: false)
+        XCTAssertEqual(count, 3)
+    }
+    
+    func testFinishTrue() {
+        XCTAssertEqual(count, 0)
+        chain.complete(with: Data(), finish: false)
+        chain.complete(with: Data(), finish: true)
+        chain.complete(with: Data())
+        XCTAssertEqual(count, 2)
+    }
+    
 }
 
 // MARK: - Result
