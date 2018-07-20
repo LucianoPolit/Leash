dcee8ec055858376b39ca037b8fa3d3b68df670a
Luciano Polit <lucianopolit@gmail.com>
2018-07-20 14:20:49 -0300

    Update source

--- Source/Core/Interceptors/InterceptorChain.swift
+++ Source/Core/Interceptors/InterceptorChain.swift
@@ -90,7 +90,7 @@ extension InterceptorChain {
     /// Completes the interception with the specified tuple.
     private func complete(_ tuple: (Response<T>, Bool)?) {
         guard !completed else { return }
-        completed = true
+        completed = tuple?.1 ?? true
         completion(tuple)
     }
     
